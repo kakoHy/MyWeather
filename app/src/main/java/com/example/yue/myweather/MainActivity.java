b55fcb4602ae1400de21e4b69030bb612a37eb74
kakoHy <460968673@qq.com>
2018-11-06 11:32:08 +0800

    hello world

--- app/src/main/java/com/example/yue/myweather/MainActivity.java
+++ app/src/main/java/com/example/yue/myweather/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        
     }
 }
